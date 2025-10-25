e6866f8d1b67afd840f302181055da2812a93505
hariompatidar04 <patidarh930@gmail.com>
2025-10-25 16:43:05 +0530

    committed Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { AiFillAccountBook } from "react-icons/ai";
+function App() {
+  return (
+    <>
+      <div className="
+      flex items-center justify-center 
+      h-screen
+      text-4xl 
+      font-bold 
+      text-white
+      bg-gray-900
+      ">
+        Welcome to Ecom Express 
+      <AiFillAccountBook />
+      </div>
+    </>
+  )
+}
+
+export default App
